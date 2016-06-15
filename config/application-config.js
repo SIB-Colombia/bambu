5eff7e1eb2e3e2133bbdb4a73e38f1463ce41156
Valentina Grajales <valegrajales@gmail.com>
2016-06-14 19:26:35 -0500

    Include linter for ES6

---
+++ config/application-config.js
@@ -0,0 +1,57 @@
+// Dependencies
+import convict from 'convict';
+import util from 'util';
+import debug from 'debug';
+
+export const config = convict({
+  env: {
+    doc: 'Application environment.',
+    format: ['production', 'development'],
+    default: 'development',
+    env: 'NODE_ENV'
+  },
+  service: {
+    name: {
+      doc: 'The name of your service/platform.',
+      default: 'Dataportal API Services',
+      env: 'SERVICE_NAME'
+    }
+  },
+  logs: {
+    doc: 'Log save location',
+    default: 'logs/dataportal-api.log',
+    env: 'LOG'
+  },
+  server: {
+    port: {
+      doc: 'The server port to bind.',
+      format: 'port',
+      default: 8000,
+      env: 'PORT'
+    }
+  },
+  database: {
+    elasticSearch: {
+      url: {
+        doc: 'ElasticSearch url to connect to (including db reference)',
+        default: 'localhost:9200',
+        env: 'ELASTICSEARCH_URL'
+      }
+    }
+  }
+});
+
+// catch all error without handler
+process.on('uncaughtException', error => {
+  debug(`Caught exception without specific handler: ${util.inspect(error)}`);
+  debug(error.stack, 'error');
+  process.exit(1);
+});
+
+// print the environment for debugging
+debug(util.inspect(process.env, {
+  colors: true
+}));
+
+// perform the config validation
+config.validate();
